9cce573d2baf4f8dfe75371f1e368b17a9dd8ff7
liammelkersson <liammelkersson@live.se>
2024-10-09 09:46:35 +0200

    boid personality + minor adjustments

--- sketch.js
+++ sketch.js
@@ -17,6 +17,11 @@ let handDetected = false;
 let obstaclePosition;
 let obstacleRadius = 40;
 
+//canvas variables
+let canvasWidth = 500; //windowWidth;
+let canvasHeight = 500; //windowHeight;
+
+//LOAD
 window.addEventListener("load", () => {
   player = new Tone.Player("/nikes.mp3");
   oscillator = new Tone.Oscillator(440, "sine").toDestination();
@@ -32,6 +37,7 @@ window.addEventListener("load", () => {
   mic.connect(analyser);
 });
 
+//CLICK
 window.addEventListener("click", () => {
   player.start();
   for (let i = 0; i < 100; i++) {
@@ -39,21 +45,24 @@ window.addEventListener("click", () => {
   }
 });
 
+//SETUP
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(24);
+  createCanvas(canvasWidth, canvasHeight);
+  // frameRate(60);
 
   video = createCapture(VIDEO);
-  video.size(windowWidth, windowHeight);
+  video.size(canvasWidth, canvasHeight);
   video.hide();
+  //https://docs.ml5js.org/#/reference/handpose
 
-  // handpose = ml5.handpose(video);
   handpose = ml5.handpose(video, modelReady);
   handpose.on("predict", (results) => {
     predictions = results;
   });
+  // https://editor.p5js.org/LillianBrevik/sketches/8ESd_N0Qh
 }
 
+//logging the handpose model
 function modelReady() {
   console.log("Handpose model ready!");
 }
@@ -70,23 +79,21 @@ function draw() {
     updateBoid(boid);
 
     let frequencyIndex = 100;
-    let frequencyValue = frequencyData[frequencyIndex];
     //this limits the frequency to 100
+    let frequencyValue = frequencyData[frequencyIndex];
 
-    boid.color = color(
-      map(frequencyValue, -100, 0, 0, 255),
-      random(255),
-      random(255)
-    );
+    // COLOR CHANGE BASED ON FRQUENCY
+    // boid.color = color(
+    //   map(frequencyValue, -100, 0, 0, 255),
+    //   random(255),
+    //   random(255)
+    // );
 
-    // change boid velocity depending of frequency from the song
-    let newVelocity = p5.Vector.random2D().setMag(
-      map(frequencyValue, -100, 0, 2, 100)
-    );
-    boid.velocity.lerp(newVelocity, 0.1);
-    // helps to smoothly interpolate the velocity changes
-    // lerp calcs the value between the new velocity and 0.1
-    // https://p5js.org/reference/p5/lerp/
+    // change boid velocity depending on frequency from the song
+    // let newVelocity = p5.Vector.random2D().setMag(
+    //   map(frequencyValue, -100, 0, 2, 100)
+    // );
+    // boid.velocity.lerp(newVelocity, 0.1);
 
     showBoid(boid);
   }
@@ -96,13 +103,37 @@ function draw() {
 
 // CREATE
 function createBoid() {
+  let behavior = "";
+  let behaviorChance = random(1);
+
+  if (behaviorChance < 0.1) {
+    behavior = "extrovert";
+  } else if (behaviorChance < 0.2) {
+    behavior = "introvert";
+  } else {
+    behavior = "normal"; // default behavior
+  }
+
+  let boidColor;
+  if (behavior === "extrovert") {
+    boidColor = color(255, 255, 0); // yellow color for extrovert behavior
+  } else if (behavior === "introvert") {
+    boidColor = color(255, 0, 0); // red color for introvert behavior
+  } else {
+    boidColor = color(random(10), random(255), random(255), random(2, 150));
+    // blue and or green color for normal behavior
+    //NOTE TO SELF: implement the frquency color change here?
+  }
+
   return {
     position: createVector(random(width), random(height)),
     velocity: p5.Vector.random2D().setMag(random(2, 4)),
     acceleration: createVector(),
+    //maxForce and maxSpeed can be adjusted to liking
     maxForce: random(0.2, 0.8),
     maxSpeed: random(2, 6),
-    color: color(random(10), random(255), random(255), random(2, 150)),
+    color: boidColor,
+    behavior: behavior,
   };
 }
 
@@ -113,13 +144,6 @@ function showBoid(b) {
 
   let angle = b.velocity.heading() + PI / 2;
   push();
-  // translate(b.position.x, b.position.y);
-  // rotate(angle);
-  // beginShape();
-  // vertex(0, -10);
-  // vertex(-5, 10);
-  // vertex(5, 10);
-  // endShape(CLOSE);
   ellipse(b.position.x, b.position.y, 3);
   pop();
 }
@@ -135,7 +159,7 @@ function flockBoids(b, boids) {
   b.acceleration.add(separation);
 
   if (obstaclePosition) {
-    let avoidance = avoidObstacle(b);
+    let avoidance = avoidObstacle(b, b.behavior);
     b.acceleration.add(avoidance);
   }
 }
@@ -247,17 +271,14 @@ function drawHand() {
   if (predictions.length > 0) {
     handDetected = true;
 
-    // we only take the first hand detected
     let hand = predictions[0];
 
-    // get the index finger tip (landmark 8)
-    // https://docs.ml5js.org/assets/handpose-keypoints-map.png
     let indexFinger = hand.landmarks[8];
 
     let x = map(indexFinger[0], 0, video.width, video.width, 0);
     let y = map(indexFinger[1], 0, video.height, 0, height);
 
-    fill(255, 0, 0);
+    fill(255, 255, 255, 80);
     noStroke();
     ellipse(x, y, obstacleRadius);
 
@@ -269,7 +290,7 @@ function drawHand() {
 }
 
 // AVOID OBSTACLE FUNCTION
-function avoidObstacle(b) {
+function avoidObstacle(b, behavior) {
   let avoidanceForce = createVector(0, 0);
   let obstacleDist = dist(
     b.position.x,
@@ -278,9 +299,20 @@ function avoidObstacle(b) {
     obstaclePosition.y
   );
 
+  //avoidance factor for the normal ones
+  let avoidanceFactor = 1.0;
+
+  if (behavior === "extrovert") {
+    avoidanceFactor = -2;
+    // extroverted boids are more attracted to the obstacle
+  } else if (behavior === "introvert") {
+    avoidanceFactor = 100.0;
+    // introverted boids are more avoidant of the obstacle
+  }
+
   if (obstacleDist < obstacleRadius + 100) {
     let diff = p5.Vector.sub(b.position, obstaclePosition);
-    diff.setMag(b.maxSpeed);
+    diff.setMag(b.maxSpeed * avoidanceFactor);
     diff.sub(b.velocity);
     diff.limit(b.maxForce);
     avoidanceForce.add(diff);
