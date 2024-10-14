b8906b035fc5c94be16785aacc961ae66751a9b1
liammelkersson <liammelkersson@live.se>
2024-10-14 14:12:18 +0200

    adjustments + UX

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ window.addEventListener("click", () => {
 //SETUP
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  // frameRate(60);
+  frameRate(80);
 
   video = createCapture(VIDEO);
   video.size(canvasWidth, canvasHeight);
